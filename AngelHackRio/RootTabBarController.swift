7f3bde8f6810a98f319855382284743e20f749b9
Jean Paul Marinho <jpsmarinho@me.com>
2016-06-26 09:54:16 -0300

    Major changes

---
+++ AngelHackRio/RootTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  RootTabBarController.swift
+//  AngelHackRio
+//
+//  Created by Jean Paul Marinho on 26/06/16.
+//  Copyright © 2016 Jean Paul Marinho. All rights reserved.
+//
+
+import UIKit
+
+class RootTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor.black()
+    }
+}
